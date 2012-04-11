a759bcf8438db7c94f0ee4dfd1341e0d12f5ea18
Colin Marc <colinmarc@gmail.com>
2012-04-10 23:53:24 -0400

    need to check if target is a dict. generalized getattr/setattr

--- zodiac.py
+++ zodiac.py
@@ -6,6 +6,25 @@ except ImportError:
 import types
 import imp
 
+_get_default = object()
+def _get(obj, name, default=_get_default):
+	if isinstance(obj, dict):
+		if default is _get_default:
+			return obj[name]
+		else:
+			return obj.get(name, default)
+	else:
+		if default is _get_default:
+			return getattr(obj, name)
+		else:
+			return getattr(obj, name, default)
+
+def _set(obj, name, val):
+	if isinstance(obj, dict):
+		obj[name] = val
+	else:
+		return setattr(obj, name, val)
+
 def _create_closure_cell(obj):
 	def ret(): obj
 	return ret.__closure__[0]
@@ -18,7 +37,7 @@ def rebase_function(f, target, new_name=None, ns=None):
 	if f.__closure__:
 		new_closure = []
 		for c in f.__closure__:
-			name = getattr(c.cell_contents, '__name__', False)
+			name = _get(c.cell_contents, '__name__', False)
 			if name and name in ns:
 				new_closure.append(_create_closure_cell(ns[name]))
 			else:
@@ -35,10 +54,7 @@ def rebase_function(f, target, new_name=None, ns=None):
 		new_closure
 	)
 	
-	if isinstance(target, dict):
-		target[new_name] = new_f
-	else:
-		setattr(target, new_name, new_f)
+	_set(target, new_name, new_f)
 
 def rebase_class(cls, target, new_name=None, ns=None):
 	if not new_name:
@@ -47,7 +63,7 @@ def rebase_class(cls, target, new_name=None, ns=None):
 
 	new_bases = []
 	for base in cls.__bases__:
-		new_base = getattr(target, base.__name__, False)
+		new_base = _get(target, base.__name__, False)
 		if new_base and isinstance(new_base, type):
 			new_bases.append(new_base)
 		else:
@@ -63,10 +79,7 @@ def rebase_class(cls, target, new_name=None, ns=None):
 		if isinstance(item, types.MemberDescriptorType): continue
 		rebase(item, new_cls, name, ns)
 
-	if isinstance(target, dict):
-		target[new_name] = new_cls
-	else:
-		setattr(target, new_name, new_cls)
+	_set(target, new_name, new_cls)
 
 def rebase(obj, target, new_name=None, ns=None):
 
@@ -75,10 +88,7 @@ def rebase(obj, target, new_name=None, ns=None):
 	elif isinstance(obj, types.FunctionType):
 		rebase_function(obj, target, new_name, ns)
 	else:
-		if isinstance(target, dict):
-			target[new_name] = obj
-		else:
-			setattr(target, new_name, obj) 
+		_set(target, new_name, obj) 
 
 def build_patch(original_module, patch_module):
 	original = __import__(original_module)
